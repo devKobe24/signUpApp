aa21afe5312290687d0bccc838bd3e2a84c9c212
Kobe <dev.skyachieve91@gmail.com>
2024-10-27 07:48:51 +0900

    줄 바꿈 수정 및 registerAdminUser 함수 작성.

--- src/main/java/com/kobe/signUpApp/controller/user/UserController.java
+++ src/main/java/com/kobe/signUpApp/controller/user/UserController.java
@@ -1,6 +1,8 @@
 package com.kobe.signUpApp.controller.user;
 
+import com.kobe.signUpApp.domain.adminUser.AdminUser;
 import com.kobe.signUpApp.domain.user.User;
+import com.kobe.signUpApp.dto.adminUser.request.AdminUserCreateRequest;
 import com.kobe.signUpApp.dto.user.request.UserCreateRequest;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,9 +15,30 @@ import java.util.List;
 public class UserController {
 
     private final List<User> users = new ArrayList<>();
+    private final List<AdminUser> adminUsers = new ArrayList<>();
 
     @PostMapping("/register")
     public void registerUser(@RequestBody UserCreateRequest request) {
-        users.add(new User(request.getUsername(), request.getEmail(), request.getPassword(), request.getConfirmPassword()));
+        users.add(
+                new User(
+                        request.getUsername(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        request.getConfirmPassword()
+                )
+        );
+    }
+
+    @PostMapping("/register/admin")
+    public void registerAdminUser(@RequestBody AdminUserCreateRequest request) {
+        adminUsers.add(
+                new AdminUser(
+                        request.getAdminName(),
+                        request.getAdminNumber(),
+                        request.getAdminPassword(),
+                        request.getAdminEmail(),
+                        request.getConfirmAdminPassword()
+                )
+        );
     }
 }
