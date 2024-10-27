bbde43fc040e9d4f56418fbd28ebe0c19a531413
Kobe <dev.skyachieve91@gmail.com>
2024-10-28 05:16:35 +0900

    registerUser 함수 수정

--- src/main/java/com/kobe/signUpApp/controller/user/UserController.java
+++ src/main/java/com/kobe/signUpApp/controller/user/UserController.java
@@ -1,44 +1,21 @@
 package com.kobe.signUpApp.controller.user;
 
-import com.kobe.signUpApp.domain.adminUser.AdminUser;
-import com.kobe.signUpApp.domain.user.User;
-import com.kobe.signUpApp.dto.adminUser.request.AdminUserCreateRequest;
 import com.kobe.signUpApp.dto.user.request.UserCreateRequest;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.kobe.signUpApp.service.user.UserService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api")
 public class UserController {
 
-    private final List<User> users = new ArrayList<>();
-    private final List<AdminUser> adminUsers = new ArrayList<>();
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @PostMapping("/register")
     public void registerUser(@RequestBody UserCreateRequest request) {
-        users.add(
-                new User(
-                        request.getUsername(),
-                        request.getEmail(),
-                        request.getPassword(),
-                        request.getConfirmPassword()
-                )
-        );
-    }
-
-    @PostMapping("/register/admin")
-    public void registerAdminUser(@RequestBody AdminUserCreateRequest request) {
-        adminUsers.add(
-                new AdminUser(
-                        request.getAdminName(),
-                        request.getAdminNumber(),
-                        request.getAdminPassword(),
-                        request.getAdminEmail(),
-                        request.getConfirmAdminPassword()
-                )
-        );
+        userService.saveUser(request);
     }
 }
