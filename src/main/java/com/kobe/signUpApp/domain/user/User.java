ef43250f17974116cf6879458fae731dffcc1178
Kobe <dev.skyachieve91@gmail.com>
2024-10-28 05:16:03 +0900

    User 클래스 수정

--- src/main/java/com/kobe/signUpApp/domain/user/User.java
+++ src/main/java/com/kobe/signUpApp/domain/user/User.java
@@ -1,31 +1,76 @@
 package com.kobe.signUpApp.domain.user;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user")
 public class User {
-    private String username;
-    private String email;
-    private String password;
-    private String confirmPassword;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_name", nullable = false)
+    private String userName;
+
+    @Column(name = "user_email", nullable = false, unique = true)
+    private String userEmail;
+
+    @Column(name = "user_password", nullable = false)
+    private String userPassword;
+    private String confirmUserPassword;
+
+    @Column(name = "password_match")
+    private boolean isSamePassword;
+
+    protected User() {}
 
     public User(
-            String username,
-            String email,
-            String password,
-            String confirmPassword
+            String userName,
+            String userEmail,
+            String userPassword,
+            String confirmUserPassword,
+            boolean isSamePassword
     ) {
-        if (username == null || username.isBlank()) {
-            throw new IllegalArgumentException(String.format("잘못된 username(%s)이 들어왔습니다.", username));
-        } else if (email == null || email.isBlank()) {
-            throw new IllegalArgumentException(String.format("잘못된 email(%s)이 들어왔습니다.", email));
-        } else if (password == null || password.isBlank()) {
-            throw new IllegalArgumentException(String.format("잘못된 password(%s)이 들어왔습니다.", password));
-        } else if (confirmPassword == null || confirmPassword.isBlank()) {
-            throw new IllegalArgumentException(String.format("잘못된 confirmPassword(%s)이 들어왔습니다.", confirmPassword));
+        if (userName == null || userName.isBlank()) {
+            throw new IllegalArgumentException(String.format("잘못된 userName(%s)이 들어왔습니다.", userName));
+        } else if (userEmail == null || userEmail.isBlank()) {
+            throw new IllegalArgumentException(String.format("잘못된 userEmail(%s)이 들어왔습니다.", userEmail));
+        } else if (userPassword == null || userPassword.isBlank()) {
+            throw new IllegalArgumentException(String.format("잘못된 userPassword(%s)이 들어왔습니다.", userPassword));
+        } else if (confirmUserPassword == null || confirmUserPassword.isBlank()) {
+            throw new IllegalArgumentException(String.format("잘못된 confirmUserPassword(%s)이 들어왔습니다.", confirmUserPassword));
         }
 
 
-        this.username = username;
-        this.email = email;
-        this.password = password;
-        this.confirmPassword = confirmPassword;
+        this.userName = userName;
+        this.userEmail = userEmail;
+        this.userPassword = userPassword;
+        this.confirmUserPassword = confirmUserPassword;
+        this.isSamePassword = isSamePassword;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public String getConfirmUserPassword() {
+        return confirmUserPassword;
+    }
+
+    public boolean isSamePassword() {
+        return isSamePassword;
     }
 }
