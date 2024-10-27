6ff4da60cc26de89282940fb81b8ca282611050e
Kobe <dev.skyachieve91@gmail.com>
2024-10-28 05:15:19 +0900

    회원가입 로직 수정

--- src/main/resources/static/v1/static/js/sign-up.js
+++ src/main/resources/static/v1/static/js/sign-up.js
@@ -1,44 +1,52 @@
 // scripts.js
-document.getElementById('signupForm').addEventListener('submit', function (event) {
-    event.preventDefault();
+document.addEventListener('DOMContentLoaded', function (){
+    document.getElementById('signupForm').addEventListener('submit', function (event) {
+        event.preventDefault();
 
-    const userName = document.getElementById('username').value;
-    const userEmail = document.getElementById('email').value;
-    const userPassword = document.getElementById('password').value;
-    const confirmPassword = document.getElementById('confirm-password').value;
+        const userName = document.getElementById('username').value;
+        const userEmail = document.getElementById('email').value;
+        const userPassword = document.getElementById('password').value;
+        const confirmUserPassword = document.getElementById('confirm-userPassword').value;
 
-    if (userPassword != confirmPassword) {
-        alert("Passwords do not match!")
-        return;
-    }
+        if (userPassword != confirmUserPassword) {
+            alert("Passwords do not match!")
+            return;
+        }
 
-    const registerData = {
-        userName: userName,
-        userEmail: userEmail,
-        userPassword: userPassword
-    };
+        const registerData = {
+            userName: userName,
+            userEmail: userEmail,
+            userPassword: userPassword,
+            confirmUserPassword: confirmUserPassword
+        };
 
-    // Fetch API를 사용하여 서버로 POST 요청을 보냄
-    fetch('/api/register', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(registerData)
-    })
-        .then(response => {
-            if (response.ok) {
-                return response.json(); // 서버에서 JSON 데이터를 반환할 경우 처리
-            }
-            throw new Error('Sign Up Failed!');
+        // Fetch API를 사용하여 서버로 POST 요청을 보냄
+        fetch('http://localhost:8081/api/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(registerData)
         })
-        .then(data => {
-            console.log('Sign Up Success: ', data);
-            alert('Sign Up Successful!');
-            // 필요한 결우 리디렉션 또는 추가 작업 수행
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            alert('Sign Up Failed.');
-        });
+            .then(response => {
+                if (response.ok) {
+                    console.log("Response Status:", response.status);
+                    console.log("Response OK:", response.ok);
+
+                    alert("회원가입에 성공하였습니다!");
+                    return;
+                } else {
+                    alert("회원가입에 실패했습니다, 다시 시도해주세요.")
+                    throw new Error('Sign Up Failed!');
+                }
+            })
+            .then(data => {
+                console.log("Response Data:", data);
+                window.location.href = "/v1/main.html";
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                alert("회원가입에 실패했습니다.")
+            });
+    });
 });
