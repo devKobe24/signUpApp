7af1b0359313eadf39d23198a696a5db44b9c9e7
Kobe <dev.skyachieve91@gmail.com>
2024-11-02 05:58:43 +0900

    JWT 유틸 관련 클래스 생성 및 구현과 관련 메서드 구현.

---
+++ src/main/java/com/kobe/signUpApp/util/JwtUtil.java
@@ -0,0 +1,53 @@
+package com.kobe.signUpApp.util;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    // 로깅을 위한 Logger 객체 생성.
+    private static final Logger logger = LoggerFactory.getLogger(JwtUtil.class);
+
+    private static final String SECRET_KEY = "Xi+bKuqLDG1tTXS8wyQ7IkFMSrpt+J9EAsM7bIBAQTGYPBj8xhaPKWhh0QBLfSjrGfYH1q+7FoXsKUXv4iHO5A==";
+    private static final long EXPIRATION_TIME = 86400000; // 1일
+
+    private static SecretKey getSigningKey() {
+        byte[] keyBytes = io.jsonwebtoken.io.Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    // JWT 토큰 생성 메서드
+    public static String generateToken(String email) {
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS512)
+                .compact();
+    }
+
+    // JWT 토큰 검증 및 파싱 메서드
+    public static String validateTokenAndGetEmail(String token) {
+        try {
+            return Jwts.parser()
+                    .verifyWith(getSigningKey())
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload()
+                    .getSubject();
+        } catch (JwtException e) {
+            // 예외를 로깅
+            logger.error("JWT validation failed: {}", e.getMessage(), e);
+            return null;
+        }
+    }
+}
