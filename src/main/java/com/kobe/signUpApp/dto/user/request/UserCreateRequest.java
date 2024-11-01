4b9ba14999fa4fd0880127047d1331b01cd986a0
Kobe <dev.skyachieve91@gmail.com>
2024-11-02 05:54:14 +0900

    일관성 없는 네이밍 수정.

--- src/main/java/com/kobe/signUpApp/dto/user/request/UserCreateRequest.java
+++ src/main/java/com/kobe/signUpApp/dto/user/request/UserCreateRequest.java
@@ -1,31 +1,31 @@
 package com.kobe.signUpApp.dto.user.request;
 
 public class UserCreateRequest {
-    private String userName;
-    private String userEmail;
-    private String userPassword;
-    private String confirmUserPassword;
+    private String name;
+    private String email;
+    private String password;
+    private String confirmPassword;
     private Boolean isSamePassword;
 
-    public String getUserName() {
-        return userName;
+    public String getName() {
+        return name;
     }
 
-    public String getUserEmail() {
-        return userEmail;
+    public String getEmail() {
+        return email;
     }
 
-    public String getUserPassword() {
-        return userPassword;
+    public String getPassword() {
+        return password;
     }
 
-    public String getConfirmUserPassword() {
-        return confirmUserPassword;
+    public String getConfirmPassword() {
+        return confirmPassword;
     }
 
     // 패스워드 확인 로직
     public Boolean getSamePassword() {
-        if (this.userPassword.equals(this.confirmUserPassword)) {
+        if (this.password.equals(this.confirmPassword)) {
             return this.isSamePassword = true;
         } else {
             return this.isSamePassword = false;
