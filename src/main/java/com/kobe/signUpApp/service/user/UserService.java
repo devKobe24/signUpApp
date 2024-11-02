37d44fc34036ec7edfc76da9eeeaeb21345b4b31
Kobe <dev.skyachieve91@gmail.com>
2024-11-03 07:39:58 +0900

    PasswordEncoder 구현, 유저 인증 로직 수정.

--- src/main/java/com/kobe/signUpApp/service/user/UserService.java
+++ src/main/java/com/kobe/signUpApp/service/user/UserService.java
@@ -1,9 +1,11 @@
 package com.kobe.signUpApp.service.user;
 
+import com.kobe.signUpApp.domain.user.User;
 import com.kobe.signUpApp.dto.user.request.UserCreateRequest;
 import com.kobe.signUpApp.dto.user.response.UserResponse;
 import com.kobe.signUpApp.repository.user.UserJdbcRepository;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -13,36 +15,44 @@ public class UserService {
 
     private final UserJdbcRepository userJdbcRepository;
     private final JdbcTemplate jdbcTemplate;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserJdbcRepository userJdbcRepository, JdbcTemplate jdbcTemplate) {
+    public UserService(
+            UserJdbcRepository userJdbcRepository,
+            JdbcTemplate jdbcTemplate,
+            PasswordEncoder passwordEncoder
+    ) {
         this.userJdbcRepository = userJdbcRepository;
         this.jdbcTemplate = jdbcTemplate;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public void saveUser(UserCreateRequest request) {
+        String encodedPassword = passwordEncoder.encode(request.getUserPassword());
+
         userJdbcRepository.saveUser(
-                request.getName(),
-                request.getEmail(),
-                request.getPassword(),
+                request.getUserName(),
+                request.getUserEmail(),
+                encodedPassword,
                 request.getSamePassword()
                 );
     }
 
     // 유저 인증 로직
-    public UserResponse authenticateUser(String email, String password) {
-        Optional<UserResponse> userOptional = userJdbcRepository.getUserByEmail(email);
+    public UserResponse authenticateUser(String email, String rawPassword) {
+        Optional<User> userOptional = userJdbcRepository.getUserWithPasswordByEmail(email);
         if (userOptional.isPresent()) {
-            UserResponse user = userOptional.get();
-            if (user.getPassword().equals(password)) {
-                System.out.println("로그인 성공: " + email); // 디버깅 로그
-                return user;
+            User user = userOptional.get();
+            if (passwordEncoder.matches(rawPassword, user.getUserPassword())) {
+                // 비밀번호가 일치하면 UserResponse로 필요한 정보만 변환.
+                return new UserResponse(user.getId(), user.getUserName(), user.getUserEmail(), "");
             } else {
-                System.out.println("비밀번호 불일치: " + email); // 디버깅 로그
+                System.out.println("비밀번호 불일치");
             }
         } else {
-            System.out.println("이메일 불일치: " + email); // 디버깅 로그
+            System.out.println("이메일 불일치: " + email);
         }
-        return null; // 인증 실패 시 null 반환.
+        return null; // 인증 실패 시 null 반환
     }
 
     // 이메일로 사용자 정보 조회 메서드
