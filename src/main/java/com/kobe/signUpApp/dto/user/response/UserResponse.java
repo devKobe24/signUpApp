bf27b0b3cd61b72b19bb67683a23ea31c5cef762
Kobe <dev.skyachieve91@gmail.com>
2024-11-03 07:39:04 +0900

    password 필드 삭제.

--- src/main/java/com/kobe/signUpApp/dto/user/response/UserResponse.java
+++ src/main/java/com/kobe/signUpApp/dto/user/response/UserResponse.java
@@ -1,29 +1,24 @@
 package com.kobe.signUpApp.dto.user.response;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.kobe.signUpApp.domain.user.User;
 import lombok.Data;
 
 @Data
-@JsonIgnoreProperties({"password"})
 public class UserResponse {
     private long id;
     private String name;
     private String email;
-    private String password;
     private String confirmPassword;
 
     public UserResponse(
             long id,
             String name,
             String email,
-            String password,
             String confirmPassword
     ) {
         this.id = id;
         this.name = name;
         this.email = email;
-        this.password = password;
         this.confirmPassword = confirmPassword;
     }
 
@@ -31,7 +26,6 @@ public class UserResponse {
         this.id = id;
         this.name = user.getUserName();
         this.email = user.getUserEmail();
-        this.password = user.getUserPassword();
         this.confirmPassword = user.getConfirmUserPassword();
     }
 
@@ -47,9 +41,6 @@ public class UserResponse {
         return email;
     }
 
-    public String getPassword() {
-        return password;
-    }
 
     public String getConfirmPassword() {
         return confirmPassword;
