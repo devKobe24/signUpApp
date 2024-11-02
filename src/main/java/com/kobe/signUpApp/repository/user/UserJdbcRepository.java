de66221ee9224fedce1378925715fcb68cd9fa91
Kobe <dev.skyachieve91@gmail.com>
2024-11-03 07:38:44 +0900

    잘못된 형식의 sql 수정 및 이메일로 특정 유저 정보 조회(비밀번호 포함)기능을 하는 getUserWithPasswordByEmail 메서드 생성 및 구현.

--- src/main/java/com/kobe/signUpApp/repository/user/UserJdbcRepository.java
+++ src/main/java/com/kobe/signUpApp/repository/user/UserJdbcRepository.java
@@ -1,5 +1,6 @@
 package com.kobe.signUpApp.repository.user;
 
+import com.kobe.signUpApp.domain.user.User;
 import com.kobe.signUpApp.dto.user.response.UserResponse;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -29,14 +30,13 @@ public class UserJdbcRepository {
 
     // 전체 유저 정보 조회
     public List<UserResponse> getUsers() {
-        String sql = "SELECT * FROM user";
+        String sql = "SELECT id, user_name, user_email, password_match FROM user";
         return jdbcTemplate.query(sql, (rs, rowNum) -> {
             long id = rs.getLong("id");
             String userName = rs.getString("user_name");
             String userEmail = rs.getString("user_email");
-            String userPassword = rs.getString("user_password");
             String confirmUserPassword = rs.getString("password_match");
-            return new UserResponse(id, userName, userEmail, userPassword, confirmUserPassword);
+            return new UserResponse(id, userName, userEmail, confirmUserPassword);
         });
     }
 
@@ -48,12 +48,28 @@ public class UserJdbcRepository {
                 long id = rs.getLong("id");
                 String userName = rs.getString("user_name");
                 String userEmail = rs.getString("user_email");
-                String userPassword = rs.getString("user_password");
                 String confirmUserPassword = rs.getString("password_match");
-                return new UserResponse(id, userName, userEmail, userPassword, confirmUserPassword);
+                return new UserResponse(id, userName, userEmail, confirmUserPassword);
             }));
         } catch (EmptyResultDataAccessException e) {
             return Optional.empty(); // 결과가 없을 때 Optional.empty() 반환.
+        }
+    }
+
+    // 이메일로 특정 유저 정보 조회(비밀번호 포함)
+    public Optional<User> getUserWithPasswordByEmail(String email) {
+        String sql = "SELECT id, user_name, user_email, user_password FROM user WHERE user_email = ?";
+
+        try {
+            return Optional.ofNullable(jdbcTemplate.queryForObject(sql, new Object[]{email}, (rs, rowNum) -> {
+                long id = rs.getLong("id");
+                String userName = rs.getString("user_name");
+                String userEmail = rs.getString("user_email");
+                String userPassword = rs.getString("user_password");
+                return new User(id, userName, userEmail, userPassword);
+            }));
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
         }
     }
 }
