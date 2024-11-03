3f168b4b20bde7bcef575e8582aee49c4c41639d
Kobe <dev.skyachieve91@gmail.com>
2024-11-04 07:51:56 +0900

    패스워드 확인 로직 작성.

--- src/main/java/com/kobe/signUpApp/dto/admin/request/AdminCreateRequest.java
+++ src/main/java/com/kobe/signUpApp/dto/admin/request/AdminCreateRequest.java
@@ -1,11 +1,16 @@
-package com.kobe.signUpApp.dto.adminUser.request;
+package com.kobe.signUpApp.dto.admin.request;
 
-public class AdminUserCreateRequest {
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class AdminCreateRequest {
     private String adminName;
     private String adminNumber;
     private String adminPassword;
     private String adminEmail;
     private String confirmAdminPassword;
+
+    @JsonIgnore
+    private Boolean isSamePassword;
 
     public String getAdminName() {
         return adminName;
@@ -25,5 +30,14 @@ public class AdminUserCreateRequest {
 
     public String getConfirmAdminPassword() {
         return confirmAdminPassword;
+    }
+
+    // 패스워드 확인 로직
+    public Boolean getIsSamePassword() {
+        if (this.adminPassword.equals(this.confirmAdminPassword)) {
+            return this.isSamePassword = true;
+        } else {
+            return this.isSamePassword = false;
+        }
     }
 }
