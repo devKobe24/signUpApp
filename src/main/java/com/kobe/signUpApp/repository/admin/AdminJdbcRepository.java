76f02e5cd3ee68f966e1c0697e794cbd70605211
Kobe <dev.skyachieve91@gmail.com>
2024-11-04 13:46:28 +0900

    잘못된 타입 수정 및 어드민 번호로 특정 어드민 정보 조회 메서드 생성

--- src/main/java/com/kobe/signUpApp/repository/admin/AdminJdbcRepository.java
+++ src/main/java/com/kobe/signUpApp/repository/admin/AdminJdbcRepository.java
@@ -1,5 +1,6 @@
 package com.kobe.signUpApp.repository.admin;
 
+import com.kobe.signUpApp.domain.admin.Admin;
 import com.kobe.signUpApp.dto.admin.response.AdminResponse;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -37,11 +38,31 @@ public class AdminJdbcRepository {
                 String adminName = rs.getString("admin_name");
                 String adminEmail = rs.getString("admin_email");
                 String adminNumber = rs.getString("admin_number");
-                Boolean confirmPassword = rs.getBoolean("password_match");
+                String confirmPassword = rs.getString("password_match");
                 return new AdminResponse(id, adminName, adminEmail, adminNumber, confirmPassword);
             }));
         } catch (EmptyResultDataAccessException e) {
             return Optional.empty(); // 결과가 없을 때 Optional.empty() 반환
+        }
+    }
+
+    // 어드민 번호로 특정 어드민 정보 조회(비밀번호 포함)
+    public Optional<Admin> getAdminWithPasswordByAdminNumber(String number) {
+        String sql = "SELECT id, admin_name, admin_number, admin_email, admin_password FROM admin WHERE admin_number = ?";
+
+        try {
+            return Optional.ofNullable(jdbcTemplate.queryForObject(sql, new Object[]{number}, (rs, rowNum) -> {
+                long id = rs.getLong("id");
+                String adminName = rs.getString("admin_name");
+                String adminNumber = rs.getString("admin_number");
+                String adminEmail = rs.getString("admin_email");
+                String adminPassword = rs.getString("admin_password");
+
+
+                return new Admin(id, adminName, adminNumber, adminEmail, adminPassword);
+            }));
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
         }
     }
 }
