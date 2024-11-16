d37a09d3f2647ce096b91bd6b8a3e7454e76e87b
Kobe <dev.skyachieve91@gmail.com>
2024-11-17 08:28:48 +0900

    UserSaveHistoryRepository 유저 회원 가입 기록 레포지토리 생성 및 구현

---
+++ src/main/java/com/kobe/signUpApp/domain/user/saveHistory/UserSaveHistoryRepository.java
@@ -0,0 +1,7 @@
+package com.kobe.signUpApp.domain.user.saveHistory;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserSaveHistoryRepository extends JpaRepository<UserSaveHistory, Long> {
+
+}
