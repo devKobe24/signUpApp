6b7c2c269e93f8bdcff491ed3096fff4e0b501a7
Kobe <dev.skyachieve91@gmail.com>
2024-11-17 08:24:54 +0900

    UserController 리팩토링

--- src/main/java/com/kobe/signUpApp/controller/user/UserController.java
+++ src/main/java/com/kobe/signUpApp/controller/user/UserController.java
@@ -4,12 +4,10 @@ import com.kobe.signUpApp.dto.user.request.UserCreateRequest;
 import com.kobe.signUpApp.dto.user.request.UserLoginRequest;
 import com.kobe.signUpApp.dto.user.response.UserResponse;
 import com.kobe.signUpApp.service.user.UserService;
-import com.kobe.signUpApp.util.JwtUtil;
-import org.springframework.http.HttpStatus;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -23,52 +21,22 @@ public class UserController {
     }
 
     @PostMapping("/register")
-    public void registerUser(@RequestBody UserCreateRequest request) {
-        userService.saveUser(request);
+    public UserResponse registerUser(@RequestBody UserCreateRequest request, HttpServletRequest httpRequest) {
+        return userService.saveUser(request, httpRequest);
     }
 
     @PostMapping("/login")
     public ResponseEntity<Map<String, String>> loginUser(@RequestBody UserLoginRequest request) {
-        System.out.println("Received login request for email: " + request.getUserEmail());
-        UserResponse user = userService.authenticateUser(request.getUserEmail(), request.getUserPassword());
-        if (user != null) {
-            String token = JwtUtil.generateToken(request.getUserEmail()); // jwtUtil 인스턴스로 메서드 호출
-            Map<String, String> response = new HashMap<>();
-            response.put("token", token);
-            response.put("message", "Login successful");
-            return ResponseEntity.ok(response); // JSON 형태로 토큰과 메시지 반환
-        } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of("message", "Invalid credentials"));
-        }
+        return userService.loginUser(request, userService);
     }
 
     @GetMapping("/user-info")
     public ResponseEntity<UserResponse> getUserInfo(@RequestHeader("Authorization") String authHeader) {
-        String token = authHeader.replace("Bearer ", "");
-        String email = JwtUtil.validateTokenAndGetEmail(token);
-
-        if (email != null) {
-            UserResponse user = userService.getUserByEmail(email);
-            return ResponseEntity.ok(user);
-        } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
+        return userService.userInfo(authHeader, userService);
     }
 
     @PostMapping("/sign-in")
     public ResponseEntity<Map<String, String>> signIn(@RequestBody UserCreateRequest request) {
-        UserResponse user = userService.authenticateUser(request.getUserEmail(), request.getUserPassword());
-        if (user != null) {
-            // 토큰 생성
-            String token = JwtUtil.generateToken(user.getEmail());
-
-            Map<String, String> response = new HashMap<>();
-            response.put("email", user.getEmail());
-            response.put("token", token);
-
-            return ResponseEntity.ok(response); // JSON 형태로 email과 token 반환
-        } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of("message", "Invalid credentials"));
-        }
+        return userService.signIn(request, userService);
     }
 }
